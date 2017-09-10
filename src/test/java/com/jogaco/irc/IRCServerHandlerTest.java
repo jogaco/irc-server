4362eec276bb12de3d5e296ed5eabc3ba6157cb4
jogaco <jogaco.en@gmail.com>
2017-09-10 12:04:54 +0200

    Added test for max users after leaving

--- src/test/java/com/jogaco/irc/IRCServerHandlerTest.java
+++ src/test/java/com/jogaco/irc/IRCServerHandlerTest.java
@@ -244,6 +244,44 @@ public class IRCServerHandlerTest {
         assertThat(response, is(ChannelMaxUsersException.TOO_MANY_USERS));
     }
 
+    
+    @Test
+    public void handleJoinChannelMaxClientsAcceptAfterLeave() {
+        ServerContext serverContext = new IRCServer(1);
+        
+        User user = new User("user", "user");
+        IRCServerHandler handler = new IRCServerHandler(serverContext);
+        IRCServerHandler handlerMock = spy(handler);
+        when(handlerMock.getUser()).thenReturn(user);
+        EmbeddedChannel channel = new EmbeddedChannel(handlerMock);
+        channel.writeInbound(Unpooled.wrappedBuffer("/join channel".getBytes()));
+
+        Chat chat = serverContext.getOrCreateChat("channel");
+        Chat chatMock = spy(chat);
+        when(chatMock.maxClientsPerChannel()).thenReturn(2);
+        ServerContext serverContextMock = spy(serverContext);
+        when(serverContextMock.getOrCreateChat("channel")).thenReturn(chatMock);
+        
+        User user2 = new User("user2", "user2");
+        IRCServerHandler handler2 = new IRCServerHandler(serverContextMock);
+        IRCServerHandler handlerMock2 = spy(handler2);
+        when(handlerMock2.getUser()).thenReturn(user2);
+        EmbeddedChannel channel2 = new EmbeddedChannel(handlerMock2);
+        channel2.writeInbound(Unpooled.wrappedBuffer("/join channel".getBytes()));
+
+        channel.writeInbound(Unpooled.wrappedBuffer("/leave".getBytes()));
+        
+        User user3 = new User("user3", "user3");
+        IRCServerHandler handler3 = new IRCServerHandler(serverContextMock);
+        IRCServerHandler handlerMock3 = spy(handler3);
+        when(handlerMock3.getUser()).thenReturn(user3);
+        EmbeddedChannel channel3 = new EmbeddedChannel(handlerMock3);
+        channel3.writeInbound(Unpooled.wrappedBuffer("/join channel".getBytes()));
+        
+        chat = serverContext.getOrCreateChat("channel");
+        final List<User> users = chat.getUsers();
+        assertThat(users, hasItems(user2, user3));
+    }
 
     @Test
     public void handleUsers() {
